2b3c291b8b8b67bc24d25bc38417f9cb392c3b6f
lembarek <lembarek2011@gmail.com>
2015-11-24 13:49:23 +0100

    fix bugs

--- app/views/layout/main.blade.php
+++ app/views/layout/main.blade.php
@@ -1,13 +1,21 @@
             @include('layout.include.head')
             @if(isset($info))
-	             <div class=" col-sm-8 col-sm-offset-2 btn btn-warning">
+            <div class="row">
+	             <div class=" col-sm-8 col-sm-offset-2">
+                            <div class="btn btn-warning">
 	                            {{ $info }}
+                            </div>
 	             </div>
+            </div>
             @endif
             @if(Session::has('global'))
-                    <div class=" col-sm-8 col-sm-offset-2 btn btn-warning">
-                            {{ Session::get('global') }}
+            <div class="row">
+                    <div class=" col-sm-8 col-sm-offset-2">
+                            <div class="btn btn-warning">
+                                {{ Session::get('global') }}
+                            </div>
                     </div>
+            </div>
             @endif
             @yield('content')
             @include('layout.include.footer')
