5b64716214d7247d29c520e434cb4ab1b85566f9
lembarek <lembarek2011@gmail.com>
2015-11-25 07:31:31 +0100

    change the jumbotron to panel in homepage

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,29 +3,45 @@
 @section('content')
 	<div class="row row-eq-height">
 		<div id="buy"  class="col-sm-6 col-md-5 col-lg-4  col-md-offset-1 col-lg-offset-2" >
-			<div class="jumbotron">
+			<div class="panel panel-default">
 				@if($buy == "yes")
-					<h1>{{ Lang::get('ccp.buy_h1') }} </h1>
-					<p> {{ Lang::get('ccp.buy_desc') }} </p>
-			 		<p><a class="btn btn-primary btn-block"  href="{{ URL::route('buy') }}" >{{ Lang::get('ccp.buy') }}</a></p>
+                                        <div class="panel-heading">
+					    <h2>{{ Lang::get('ccp.buy_h1') }} </h2>
+                                        </div>
+                                        <div class="panel-body">
+					    <p> {{ Lang::get('ccp.buy_desc') }} </p>
+			 		    <p><a class="btn btn-primary btn-block"  href="{{ URL::route('buy') }}" >{{ Lang::get('ccp.buy') }}</a></p>
+                                        </div>
 			 	@elseif($buy == "no")
-			 		<h1>{{ Lang::get('ccp.buy_h1_try_later') }}</h1>
-			 		<p> {{ Lang::get('ccp.buy_desc_try_later') }} </p>
-			 		<p><a class="btn btn-warning btn-block">{{ Lang::get('ccp.try_after_24')  }}</a></p>
+                                        <div class="panel-heading">
+			 		    <h2>{{ Lang::get('ccp.buy_h1_try_later') }}</h2>
+                                        </div>
+                                        <div class="panel-body">
+			 		    <p> {{ Lang::get('ccp.buy_desc_try_later') }} </p>
+			 		    <p><a class="btn btn-warning btn-block">{{ Lang::get('ccp.try_after_24')  }}</a></p>
+                                        </div>
 			 	@endif
 			 </div>
 		</div>
 
 		<div id="sell" class="col-sm-6 col-md-5 col-lg-4">
-			<div class="jumbotron">
+			<div class="panel panel-default">
 				@if($sell == "yes")
-					<h1> {{ Lang::get('ccp.sell_h1') }} </h1>
-					<p> {{ Lang::get('ccp.sell_desc') }} </p>
-					<p><a class="btn btn-primary btn-block" href="{{ URL::route('sell') }}" >{{ Lang::get('ccp.sell') }}</a></p>
+                                        <div class="panel-heading">
+					    <h2> {{ Lang::get('ccp.sell_h1') }} </h2>
+                                        </div>
+                                        <div class="panel-body">
+					    <p> {{ Lang::get('ccp.sell_desc') }} </p>
+					    <p><a class="btn btn-primary btn-block" href="{{ URL::route('sell') }}" >{{ Lang::get('ccp.sell') }}</a></p>
+                                        </div>
 				@elseif($sell == "no")
-					<h1> {{ Lang::get('ccp.sell_h1_try_later') }} </h1>
-					<p> {{ Lang::get('ccp.sell_desc_try_later') }} </p>
-					<p><a class="btn btn-warning btn-block">{{ Lang::get('ccp.try_after_24') }}</a> </p>
+                                        <div class="panel-heading">
+					    <h2> {{ Lang::get('ccp.sell_h1_try_later') }} </h2>
+                                        </div>
+                                        <div class="panel-body">
+					    <p> {{ Lang::get('ccp.sell_desc_try_later') }} </p>
+					    <p><a class="btn btn-warning btn-block">{{ Lang::get('ccp.try_after_24') }}</a> </p>
+                                        </div>
 				@endif
 			</div>
 		</div>
