1971bd82067702b0f413604679bc3c66ae4e1e66
lembarek <lembarek2011@gmail.com>
2015-11-27 10:11:20 +0100

    fix navbar when change the lang between ar and en

--- app/views/layout/include/navigation.blade.php
+++ app/views/layout/include/navigation.blade.php
@@ -8,12 +8,12 @@
 	  </button>
 	</div>
 	<div class="collapse navbar-collapse" id="collapse">
-	    <ul class="nav  navbar-nav nav-responsive navbar-left">
+        <ul class="nav  navbar-nav nav-responsive <?php if(Session::get('locale') == 'ar') echo 'navbar-right'; else echo 'navbar-left'; ?>">
 	        <li><a href="{{ URL::route('home') }}">{{ Lang::get('general.home') }}</a></li>
 	        <li><a href="{{ URL::route('info') }}">{{ Lang::get('general.info') }}</a></li>
 	    </ul>
 
-            <ul class="nav  navbar-nav nav-responsive navbar-right">
+            <ul class="nav  navbar-nav nav-responsive  <?php if(Session::get('locale') == 'ar') echo 'navbar-left'; else echo 'navbar-right'; ?>">
 	        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">{{  Lang::get('general.lang')  }} <b class="caret"></b></a>
     			<ul class="dropdown-menu">
 		    			<li><a href="{{ URL::route('lang_chooser') }}?locale=en">{{ Lang::get('lang.en') }}</a></li>
