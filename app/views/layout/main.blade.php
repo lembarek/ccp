c848db8918fe7fc03b2cb62755a256e7881d367d
lembarek <lembarek2011@gmail.com>
2015-11-29 05:03:07 +0100

    -fix the footer when browse using phone

--- app/views/layout/main.blade.php
+++ app/views/layout/main.blade.php
@@ -1,23 +1,25 @@
-        @include('layout.include.head')
-        <div class="container">
-            @if(isset($info))
-            <div class="row">
-	             <div class=" col-sm-8 col-sm-offset-2">
-                            <div class="btn btn-block btn-info">
-	                            {{ $info }}
-                            </div>
-	             </div>
-            </div>
-            @endif
-            @if(Session::has('global'))
-            <div class="row">
-                    <div class=" col-sm-8 col-sm-offset-2">
-                            <div class="btn btn-block btn-info">
-                                {{ Session::get('global') }}
-                            </div>
-                    </div>
-            </div>
-            @endif
-            @yield('content')
+@include('layout.include.head')
+<div class="wrap">
+    <div class="container">
+        @if(isset($info))
+        <div class="row">
+                <div class=" col-sm-8 col-sm-offset-2">
+                        <div class="btn btn-block btn-info">
+                                {{ $info }}
+                        </div>
+                </div>
         </div>
-        @include('layout.include.footer')
+        @endif
+        @if(Session::has('global'))
+        <div class="row">
+                <div class=" col-sm-8 col-sm-offset-2">
+                        <div class="btn btn-block btn-info">
+                            {{ Session::get('global') }}
+                        </div>
+                </div>
+        </div>
+        @endif
+        @yield('content')
+    </div>
+</div>
+@include('layout.include.footer')
