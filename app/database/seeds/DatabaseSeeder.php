a5e3b07cdc17a901b4810364c46e17337c6956d9
lembarek <lembarek2011@gmail.com>
2015-12-01 13:01:58 +0100

    improve db:seed

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -1,10 +1,34 @@
 <?php
 
 class DatabaseSeeder extends Seeder {
+
+        protected $tables = [
+        'users',
+        'roles',
+        'role_user',
+        ];
+
+
 	public function run()
 	{
 		Eloquent::unguard();
 
-                $this->call('SellTableSeeder');
+                $this->cleanDatabase();
+
+                foreach ($this->tables as $seedClass) {
+                    $this->call(ucfirst(camel_case($seedClass)).'TableSeeder');
+                }
 	}
+
+
+        public function cleanDatabase()
+        {
+            DB::statement('SET FOREIGN_KEY_CHECKS=0');
+            foreach ($this->tables as $table) {
+                DB::table($table)->truncate();
+            }
+
+            DB::statement('SET FOREIGN_KEY_CHECKS=1');
+        }
+
 }
